35e79f2c1df7665a26095a34ba47d5a93b98132d
jintaoju <jintaoju@jintaoju-ThinkPad-Edge-E431.(none)>
2014-05-04 19:51:25 +0800

    fix can not create

--- zookeeper/zkClient.c
+++ zookeeper/zkClient.c
@@ -15,19 +15,31 @@ int main(int argc, char** argv)
 	strcpy(p, "dummy");
 	zoo_set_debug_level(ZOO_LOG_LEVEL_DEBUG);
 	zh = zookeeper_init("localhost:2181", watcher, 10000,0,0,0);
-	if(zoo_add_auth(zh,"foo",p,strlen(p),0,0)!=ZOK)
+	while(zoo_state(zh) == 0);
+	if(zoo_add_auth(zh,"digest",p,strlen(p),0,0) != ZOK)
+	{
+		printf("auto failed\n");
 		return 2;
-	int rc = zoo_create(zh, "/xyz","value",5,&ZOO_OPEN_ACL_UNSAFE, ZOO_EPHEMERAL,buffer, sizeof(buffer) -1);
+	}
+
+	printf("auto success\n");
+	struct ACL CREATE_ONLY_ACL[] = {{ZOO_PERM_CREATE, ZOO_AUTH_IDS}};
+	struct ACL_vector CREATE_ONLY = {1, CREATE_ONLY_ACL};
+	int rc = zoo_create(zh, "/zookeeper/xyz","value",5,&ZOO_OPEN_ACL_UNSAFE, ZOO_EPHEMERAL,buffer, sizeof(buffer) -1);
 
 	int buflen = sizeof(buffer);
 	struct Stat stat;
-	rc = zoo_get(zh, "/xyz", 0, buffer, &buflen, &stat);
-	if(rc)
+	while(true)
 	{
-		fprintf(stderr, "Error %d, for %d\n",rc, __LINE__);
-	}
+		rc = zoo_get(zh, "/zookeeper/xyz", 0, buffer, &buflen, &stat);
+		if(rc)
+		{
+			fprintf(stderr, "Error %d, for %d\n",rc, __LINE__);
+		}
 
-	printf("%s", buffer);
-	sleep(100000);
+		printf("%s", buffer);
+		sleep(5);
+	}
+	zookeeper_close(zh);
 	return 0;
 }
